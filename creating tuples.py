0a66f745f0631876373fc1b647d48b0384783e49
keval joshi <61350725+kj-95@users.noreply.github.com>
2021-03-11 20:31:52 +0530

    Add files via upload

---
+++ creating tuples.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+new_tup = ()
+type(new_tup)
+
+
+# In[1]:
+
+
+new_tup = (910 , 20 , 30 ,40)
+type(new_tup)
+
+
+# In[ ]:
+
+
+#create tuples of mix datatype
+
+new_tup=( 10,20,30), (10,.1,20.2) , "ten , ")   # nested tuple
+          
+
+
+# In[2]:
+
+
+#return item at 0 th index
+new_tup[0]
+
+
+# In[3]:
+
+
+#len of tuple
+len(new_tup)
+
+
+# In[4]:
+
+
+#creating & printing sets
+
+new_set= {}
+type(new_set)
+
+
+# In[ ]:
+
+
+new_set = {'neo' , 'keval', 'kushal'}
+type(new_set)
+
+
+# In[7]:
+
+
+x_set = set('abcde')
+y_set = set('cdfg')
+
+print(x_set)
+print(y_set)
+
+
+# In[ ]:
+
+
+x.union (y)
+x_set.union(y_set)
+
+
+# In[ ]:
+
+
+# intersection(y) return common items that are present in two sets
+x_set.intersection(y_set)
+
+
+# In[ ]:
+
+
+x_set.difference(y_set)
+
+
+# In[ ]:
+
+
+x_set.difference_update(y_set)
+print(x_test)
+print(y_test)
+
+
+# In[ ]:
+
+
+x_set.disjoint(y_set)
+
+
+# In[ ]:
+
+
+y_sybset.issubset(x_set) # returns true for set 2 if all elements of sect 2 are present in set 1
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
